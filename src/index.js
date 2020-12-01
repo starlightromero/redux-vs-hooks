84845704d02057c82afeceb94df214645aa943c7
Starlight Romero <starlightromero@gmail.com>
2020-12-01 13:58:40 -0800

    replaced ProductsProvider with configureStore hook

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import ProductsProvider from './context/ProductsContext'
+import configureStore from './hooks-store/products'
+
+configureStore()
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 )
