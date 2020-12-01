728386a732d967dbbc1d2a14404ca82fb170360a
Starlight Romero <starlightromero@gmail.com>
2020-12-01 15:49:16 -0800

    added React memo to only render component when needed

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -3,8 +3,8 @@ import Card from '../UI/Card'
 import './ProductItem.css'
 import { useStore } from '../../hooks-store/store'
 
-const ProductItem = props => {
-  const dispatch = useStore[1]
+const ProductItem = React.memo(props => {
+  const dispatch = useStore(false)[1]
 
   const toggleFavHandler = () => {
     dispatch('TOGGLE_FAV', props.id)
@@ -24,6 +24,6 @@ const ProductItem = props => {
       </div>
     </Card>
   )
-}
+})
 
 export default ProductItem
